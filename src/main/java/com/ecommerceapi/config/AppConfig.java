2466b0b003ceed0421497074e9479ff49aa6f764
Prabhav Giriya <prabhavgiriya925@gmail.com>
2025-07-26 21:09:16 +0530

    deployed url of frontend

--- src/main/java/com/ecommerceapi/config/AppConfig.java
+++ src/main/java/com/ecommerceapi/config/AppConfig.java
@@ -40,7 +40,8 @@ public class AppConfig {
 				CorsConfiguration cfg = new CorsConfiguration();
 				
 				cfg.setAllowedOrigins(Arrays.asList(
-						"http://localhost:3000"
+						"http://localhost:3000",
+						"myeshop-backend.railway.internal"
 						));
 				cfg.setAllowedMethods(Collections.singletonList("*"));
 				cfg.setAllowCredentials(true);
